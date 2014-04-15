06a146eeef7a2beede90ddf4555960159a24d854
Wayne <guowangyang@gmail.com>
2014-04-15 16:03:23 +0800

    + supersu: manual install script for SuperSu

---
+++ supersu/install.sh
@@ -0,0 +1,15 @@
+temp_dir=/data/supersu
+zip_file=UPDATE-SuperSU-v1.94.zip
+busybox=/data/busybox
+
+adb shell mkdir $temp_dir
+adb push update-binary $temp_dir
+adb push $zip_file $temp_dir
+adb push busybox $busybox
+
+adb shell chmod 777 $busybox
+adb shell chmod 777 $temp_dir/update-binary
+adb shell $temp_dir/update-binary 123 $temp_dir/$zip_file
+adb shell rm -rf $temp_dir
+adb shell rm $busybox
+
